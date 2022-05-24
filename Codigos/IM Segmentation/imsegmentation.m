fafc71e5f2e01a01451b737417398828a24987aa
meilichamprz <105674984+meilichamprz@users.noreply.github.com>
2022-05-24 14:41:37 -0500

    im seg

--- Codigos/IM Segmentation/imsegmentation.m
+++ Codigos/IM Segmentation/imsegmentation.m
@@ -16,7 +16,9 @@ imshow(f,[]);
 %% Thresholding
 
 seg1 = f > 0.5;
+figure
 imshow(seg1,[])
+figure
 imshow(seg1.*f,[])
 seg1 = f < 0.75;
 imshow(seg1,[])
@@ -31,17 +33,17 @@ thr = graythresh(f)
 seg1 = f > thr;
 imshow(seg1,[])
 dxp=[0,1;-1,0];
-dyp=[1,0;0,-1];
-edgemap = abs(conv2(seg1,dxp,'same'))+abs(conv2(seg1,dyp,'same'));
-imshow(f+edgemap,[0,1]);
+dyp=[1,0;0,-1];  % gradiente en y
+edgemap = abs(conv2(seg1,dxp,'same'))+abs(conv2(seg1,dyp,'same'));    %mapa de orillas centrado
+imshow(f+edgemap,[0,1]);     % ver las orillas de la imagen, otra forma de visualizar las imagenes
 
 % Compare the otsu provided threshold vs the one you selected in the
 % preview step.
 % Do you trust the Otsu treshold?
 % Select your own image and compute the otsu threshold
 %% Kmeans segmentation
-
-[L,Centers] = imsegkmeans(int8(255*f),3);
+ % se especifica el numero de clases
+[L,Centers] = imsegkmeans(int8(255*f),3);    % el numero despues de la coma es el numero de clases
 B = labeloverlay(f,L);
 imshow(B)
 title("Labeled Image")
