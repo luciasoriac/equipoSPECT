f954293cda44dd543d4a8dba3fbfabc1bf8407c2
GraceRL <105674672+GraceRL@users.noreply.github.com>
2022-06-03 10:44:37 -0500

    Update imsegmentation.m

--- Codigos/IM Segmentation/imsegmentation.m
+++ Codigos/IM Segmentation/imsegmentation.m
@@ -4,7 +4,7 @@
     % Ana Lucía Soria Cardona A00827565
     % Marcela Enriquez López A01570502
     % Natalia Verónica Flores Del Río A01570472
-    %
+    % Graciela Alejandra Rincón Lóepz A00827270
     %
 
 %%
